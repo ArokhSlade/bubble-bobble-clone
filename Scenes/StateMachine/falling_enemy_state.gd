1140e3bd874aa3cc43ec3bea76d878f534324a48
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2024-12-27 21:21:01 +0100

    fixed bug: jump height of player2 less than player

--- Scenes/StateMachine/falling_enemy_state.gd
+++ Scenes/StateMachine/falling_enemy_state.gd
@@ -1,13 +1,22 @@
 extends EnemyState
 class_name FallingEnemyState
 
+var is_first_frame : bool
+
+func enter():
+	is_first_frame = true
 
 func physics_update(delta):
-	super(delta)		
+	super(delta)
+	
 	if not enemy.is_on_floor():
+		#if is_first_frame :
+			#is_first_frame = false
+		#else: 
 		enemy.velocity += enemy.get_gravity() * delta
 		enemy.velocity.x = move_request.x * enemy.speed
 		
+	
 	elif move_request.y != 0.0:
 			state_changed.emit(self,"jumping")
 	elif move_request.x != 0.0:		
